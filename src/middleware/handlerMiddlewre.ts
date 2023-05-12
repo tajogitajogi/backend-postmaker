e44d47abf3e9d183ea1076f4cc50c77bf1b1e2f4
tajogi <120653132+tajogitajogi@users.noreply.github.com>
2023-05-12 21:16:42 +0500

    Update handlerMiddlewre.ts

--- src/middleware/handlerMiddlewre.ts
+++ src/middleware/handlerMiddlewre.ts
@@ -1,5 +1,6 @@
 import { ApiError } from '../errors/ApiError';
 
+
 module.exports = function (err:ApiError, req, res, next) {
     if (err) {
         return res.status(err.status).json({message: err.message})
